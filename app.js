2ee2d53f6dd3641effa98c31172f98a90f85dab7
sirbucristiana <sirbucristiana5@gmail.com>
2023-01-10 23:01:35 +0200

    2nd commit

---
+++ app.js
@@ -0,0 +1,10 @@
+ let button = document.getElementById('btn');
+
+button.addEventListener('click', function(){
+    let word = document.getElementById('insert').value;
+    let count = word.length;
+    let outputDiv = document.getElementById('output');
+
+    outputDiv.innerHTML = `<h1>${count}</h1>`
+}); 
+
